35e301b8980e2540dd17c407ad3d7347701b8f20
Ricardo Diaz <ruven1390@gmail.com>
2020-05-21 19:12:11 -0600

    se agrega modulo contactos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// se agrega modulo con import 
+import{ ContactosComponent} from './contactos/contactos.component'
+import { AlumnosComponent } from './alumnos/alumnos.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'contactos' ,component: ContactosComponent },
+{path: 'alumnos' ,component: AlumnosComponent }
 
+
+
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactosComponent } from './contactos/contactos.component';
+import { AlumnosComponent } from './alumnos/alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactosComponent,
+    AlumnosComponent
   ],
   imports: [
     BrowserModule,
